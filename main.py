fe7bd0dd6ea7f6382a25d04b16c03f596de43a24
githubMateoSig <Paul.Uzhca2004@gmail.com>
2023-10-31 16:20:33 -0400

    my python

---
+++ main.py
@@ -0,0 +1,45 @@
+age = 16
+name = "Paul Mateo Uzhca Sigcha"
+other = 'pepe'
+
+print( age )
+print( name )
+print( other )
+
+ADULT_AGES = 18 
+USERAGE = 18
+if USERAGE < ADULT_AGES:
+  print ("you're a child")
+elif USERAGE > ADULT_AGES: 
+  print ("You made it")
+else:
+  print ("Right in the middle")
+
+CN = 1
+EN = 11
+
+while CN < EN: 
+  print( CN )
+  CN = CN + 1 
+for FCN in range( 1,11 ):
+  print( FCN * 10)
+
+def bark():
+  print("a dog barked woof woof woof!")
+bark()
+bark()
+bark()
+def miaw():
+  print( " a cat miawed miaw miaw miaw!")
+miaw()
+miaw()
+miaw()
+
+MFC = ( "Lays", "kachos", "doritos" )
+print( MFC[0])
+print( MFC[2])
+
+print(MFC[0])
+myGrades = (50, 100, 44.5, 99)
+
+print( myGrades[0])
